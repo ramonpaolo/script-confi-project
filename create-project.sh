145af74e0836a0200f5ac58c451fc5443a71dc66
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-04-10 13:56:24 -0300

    Add files via upload
    Added options to create project

--- create-project.sh
+++ create-project.sh
@@ -16,22 +16,52 @@ read name_project
 
 echo "O nome do projeto será: $name_project"
 
-echo -e "\n"
+echo -e "\nDeseja iniciar o git?"
+read set_git
 
+echo -e "\nDeseja usar docker?"
+read set_docker
+
+echo -e "\nDeseja usar banco de dados?"
+read set_database
+
+echo -e "\nDeseja usar cloud?"
+read set_cloud
+
+if [ $set_cloud = 'yes' ]; then
+	echo -e "\nQual cloud? (firebase/aws)"
+	read set_cloud_name
+	if [ $set_cloud_name = 'aws' ]; then
+		yarn add aws-sdk
+		yarn add @types/aws-sdk -D
+	elif [ $set_cloud_name = 'firebase' ]; then
+		yarn add firebase-admin @types/firebase-admin -D
+	fi
+fi
+		
 echo -e "\n Prepare uma xicará de café enquanto será configurado o projeto : )"
 
 mkdir $name_project && cd $name_project
 
 yarn init -y
 
-yarn add express sequelize typescript pg mongoose compression jest axios dotenv ioredis
-yarn add @types/express @types/sequelize @types/compression @types/jest @types/ioredis nodemon ts-node -D
+if [ $set_docker = 'yes' ]; then
+	mkdir docker && cd docker
+	mkdir settings
+	cd ..
+	touch docker-compose.yaml
+fi
 
-mkdir docker && cd docker
+if [ $set_database = 'yes' ]; then
+	yarn add sequelize pg mongoose ioredis
+	yarn add @types/sequelize @types/pg @types/ioredis -D
+	echo -e "declare global {\nnamespace NodeJS{\ninterface ProcessEnv{\nPORT: number;\n REDIS_HOST: string;\n REDIS_PORT: number; \n REDIS_PASSWORD: string;\n MONGODB_PASSWORD: string;\n MONGODB_USERNAME: string;}}}\nexport {}" > environment.d.ts
+elif [ $set_database = 'no' ]; then
+	echo -e "declare global {\nnamespace NodeJS{\ninterface ProcessEnv{\nPORT: number;\n}}}\nexport {}" > environment.d.ts	
+fi
 
-mkdir settings
-
-cd ..
+yarn add express typescript compression jest axios dotenv
+yarn add @types/express @types/compression @types/jest nodemon ts-node -D
 
 mkdir src && cd src
 
@@ -50,7 +80,6 @@ echo -e "import express from 'express'\nimport compression from 'compression'\n\
 
 cd ..
 
-touch docker-compose.yaml
 touch README.md
 touch LICENSE
 touch '.env'
@@ -63,22 +92,20 @@ rm MIT-LICENSE.txt
 
 yarn tsc --init
 
-echo -e "Digite o caminho para adicionar no github: \n"
-read url_project_github
+if [ $set_git = 'yes' ]; then
+	echo -e "Digite o caminho para adicionar no github: \n"
+	read url_project_github
 
-git init .
+	git init .
+	git remote add origin $url_project_github	
 
-git remote add origin $url_project_github
+	git add .
+
+	git commit -m "First commit"
+
+	git push -u origin master
+fi
 
 echo -e "/node_modules\n/dist\n.env" > .gitignore 
-echo -e "declare global {\nnamespace NodeJS{\ninterface ProcessEnv{\nPORT: number;\n REDIS_HOST: string;\n REDIS_PORT: number; \n REDIS_PASSWORD: string;\n MONGODB_PASSWORD: string;\n MONGODB_USERNAME: string;}}}\nexport {}" > environment.d.ts
-
-git add .
-
-git commit -m "First commit"
-
-git push -u origin master
 
 echo -e "\n Pode abrir o projeto e programar : )"
-
-code .
